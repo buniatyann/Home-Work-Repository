72f0de0286a1637fec537540a2339fcfeef89861
buniatyann <165073089+buniatyann@users.noreply.github.com>
2024-11-09 21:59:00 +0400

    little changes in game header

--- Synopsys/Game/game.h
+++ Synopsys/Game/game.h
@@ -6,9 +6,26 @@
 #include <stdlib.h>
 #include <vector>
 
-using namespace std;
 
 class Game {
+public:
+    Game();
+    // Makes a one step of a game given input
+    void step(char& input);
+    // move() function only moves tiles to a specified direction - 
+    // i.e. only fills up empty tiles
+    bool move(char& input, bool& valid_step);
+    // Combines adjacent tiles if they are equal
+    bool combine(char& input, bool& valid_step);
+    // Adds new tile over random empty tile
+    void add_new_tile();
+    // Checks if there exist 2048 tile on the board
+    bool check_win_status();
+    // Getter
+    int get_score();
+    // Overloaded "<<" operator for printing the board, turns and score to stdout
+    friend std::ostream& operator<<(std::ostream& outs, const Game& game);
+
 private:
     static const int WIN_NUMBER = 2048; // Tile number winning condition
     static const int BSIZE = 4; // Size of the board 4x4
@@ -18,30 +35,6 @@ private:
     int turn; // Keep track of number of turns
     int score;
     
-public:
-    Game();
-
-    // Makes a one step of a game given input
-    void step(char& input);
-
-    // move() function only moves tiles to a specified direction - 
-    // i.e. only fills up empty tiles
-    bool move(char& input, bool& valid_step);
-
-    // Combines adjacent tiles if they are equal
-    bool combine(char& input, bool& valid_step);
-
-    // Adds new tile over random empty tile
-    void add_new_tile();
-
-    // Checks if there exist 2048 tile on the board
-    bool check_win_status();
-
-    // Getter
-    int get_score();
-
-    // Overloaded "<<" operator for printing the board, turns and score to stdout
-    friend ostream& operator<<(ostream& outs, const Game& game);
 };
 
 
