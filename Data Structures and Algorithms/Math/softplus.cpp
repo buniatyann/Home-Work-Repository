20f40696ce693dc6708f08246942de51435fe486
Narek <165073089+buniatyann@users.noreply.github.com>
2025-08-27 21:47:04 +0400

    Remove Math directory

--- Data Structures and Algorithms/Math/softplus.cpp
+++
@@ -1,9 +0,0 @@
-#include <cmath>
-
-/*
-    requires C++ 20, and defined in <numbers>
-    constexpr double Eu = std::numbers::e;
-*/
-double softplus(double x) {
-    return std::log1p(std::exp(x));  // is the same as log(1 + exp(x)) and numerically stable
-}
