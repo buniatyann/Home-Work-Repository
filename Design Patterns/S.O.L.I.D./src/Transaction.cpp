b92e4de43ccb3eed56a752404c950f2dfa72a400
NarBuniatyan <bunnarek@gmail.com>
2025-09-08 20:49:43 +0400

    testing

--- Design Patterns/S.O.L.I.D./src/Transaction.cpp
+++
@@ -1,8 +0,0 @@
-#include "../include/Transactions/Transaction.hpp"
-
-Transaction::Transaction(IAccount* acc, uint64_t amount, ILogger* logger)
-    : amount_(amount), account_(acc), logger_(logger), executed_(false) {}
-
-uint64_t Transaction::getAmount() const {
-    return amount_;
-}
