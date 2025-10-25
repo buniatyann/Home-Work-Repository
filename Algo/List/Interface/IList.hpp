6981011a0b06a0998e03a1cd83fe829fa97df5f2
buniatyann <narekbuniatyan.tt255-1@polytechnic.am>
2025-10-25 15:43:46 +0400

    little changes in INode

--- Algo/List/Interface/IList.hpp
+++ Algo/List/Interface/IList.hpp
@@ -4,28 +4,44 @@
 #include <cstddef>
 #include <stdexcept>
 #include <cassert>
+#include "../Node/Interface/INode.hpp"
 
 template <typename T>
 class IList {
 public:
     virtual ~IList() = default;
+
     virtual void insert(const T& value, std::size_t pos) = 0;
     virtual void insert(T&& value, std::size_t pos) = 0;
+
     virtual void push_back(const T& value) = 0;
     virtual void push_back(T&& value) = 0;
+
     virtual void push_front(const T& value) = 0;
     virtual void push_front(T&& value) = 0;
+
     virtual void pop_back() = 0;
-    virtual void pop_front() = 0; 
+    virtual void pop_front() = 0;
+
     virtual void clear() = 0;
+
     virtual const T& front() const = 0;
-    virtual const T& back() const = 0;
     virtual T& front() = 0;
+
+    virtual const T& back() const = 0;
     virtual T& back() = 0;
+
     virtual std::size_t size() const noexcept = 0;
     virtual bool empty() const noexcept = 0;
-    virtual std::size_t find(const T& value) = 0;
-    virtual const T& at(std::size_t ind) = 0;
+
+    virtual std::size_t find(const T& value) const = 0;
+    virtual const T& at(std::size_t ind) const = 0;
+
+    virtual const INode<T>* frontNode() const = 0;
+    virtual INode<T>* frontNode() = 0;
+
+    virtual const INode<T>* backNode() const = 0;
+    virtual INode<T>* backNode() = 0;
 };
 
 #endif // ILIST_HPP
