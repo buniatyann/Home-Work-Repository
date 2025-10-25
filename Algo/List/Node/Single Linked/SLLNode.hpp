11a561b800a631b1cbfb306b9f5891f346702e6f
buniatyann <narekbuniatyan.tt255-1@polytechnic.am>
2025-10-25 15:35:43 +0400

    +sll node impl

---
+++ Algo/List/Node/Single Linked/SLLNode.hpp
@@ -0,0 +1,27 @@
+#ifndef SINGLE_LINKED_LIST_NODE_HPP
+#define SINGLE_LINKED_LIST_NODE_HPP
+
+#include "../Interface/INode.hpp"
+
+template <typename T>
+struct SLLNode : INode<T> {
+    SLLNode(const T& value = T(), INode<T>* next = nullptr) : val_(value), next(next) {} 
+    SLLNode(T&& value, INode<T>* next = nullptr) : val_(std::move(value)), next(next) {}
+
+    const T& val() const override {
+        return val_;
+    }
+
+    T& val() override {
+        return val_;
+    }
+
+    ~SLLNode() override {
+        next = nullptr;
+    }
+
+    T val_;
+    SLLNode<T>* next;
+};
+
+#endif // SINGLE_LINKED_LIST_NODE_HPP
