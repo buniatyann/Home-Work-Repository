12db4e1c4248319c3aa1b7bf47a31e21161981d5
Narek <165073089+buniatyann@users.noreply.github.com>
2025-10-28 22:22:46 +0400

    little changes in polygon

--- C++ Tasks/Comp Geometry Lib/Polygon.hpp
+++ C++ Tasks/Comp Geometry Lib/Polygon.hpp
@@ -12,29 +12,31 @@
 
 template <typename T, std::size_t N>
 class Polygon {
-public:
-    static_assert(N > 0, "N must be greater than 0");
-    static_assert(std::is_arithmetic<T>::value, "T must be arithmetic type");
+    static_assert(N == 2, "Polygon is only defined for 2D (N == 2)");
+    static_assert(std::is_arithmetic_v<T>, "T must be an arithmetic type");
 
+public:
     using Point = Point<T, N>;
     using coord_t = coord_t<T>;
     using Segment = Segment<T, N>;
 
-public:
-    Polygon();
+    Polygon() = default;
     Polygon(const Polygon& other);
     Polygon& operator=(const Polygon& other);
     Polygon(Polygon&& other) noexcept;
     Polygon& operator=(Polygon&& other) noexcept;
-    ~Polygon();
+    ~Polygon() = default;
 
     BoundingBox<T, N> boundingBox() const;
 
     Point& operator[](std::size_t ind);
     const Point& operator[](std::size_t ind) const;
+    Point& at(std::size_t ind);
+    const Point& at(std::size_t ind) const;
 
     bool empty() const;
     std::size_t size() const;
+    void clear();
 
     bool isConvex() const;
     bool contains(const Point& p) const;
@@ -48,14 +50,14 @@ public:
     std::vector<Point> vertices() const;
 
     void push_back(const Point& p);
-    void insert(const Point& p, std::size_t index);
+    void insert(std::size_t index, const Point& p);
 
 private:
     std::vector<Point> data_;
-    mutable BoundingBox<T> box_;
+    mutable BoundingBox<T, N> box_;
     mutable bool boxDirty_ = true;
 };
 
 #include "Polygon.tpp"
 
-#endif // POLYGON_HPP
+#endif
